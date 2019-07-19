327b1040a80ac59291afea8d4e30224210de0874
Rodrigo de Anhaia <rodrigoanhaia2@gmail.com>
2019-07-19 12:26:40 -0300

    v2.0

--- iReminder/ViewController.swift
+++ iReminder/ViewController.swift
@@ -7,14 +7,63 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pieChart: PieChartView!
+    
+    @IBOutlet weak var iosStepper: UIStepper!
+    @IBOutlet weak var blankStepper: UIStepper!
+    
+    var iosDataEntry = PieChartDataEntry(value: 0)
+    var blankDataEntry = PieChartDataEntry(value: 0)
+
+    var numberOfDownloadsDataEntries = [PieChartDataEntry]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pieChart.chartDescription?.text = "Data Wather"
+        
+        iosDataEntry.value = iosStepper.value
+        iosDataEntry.label = "IOS"
+        
+        blankDataEntry.value = blankStepper.value
+        blankDataEntry.label = "Vazio"
+        
+        
+        numberOfDownloadsDataEntries = [iosDataEntry, blankDataEntry]
+        
+        updateChartData()
+        
     }
 
-
+    @IBAction func iosChanger(_ sender: UIStepper) {
+        iosDataEntry.value = sender.value
+        updateChartData()
+        
+        
+    }
+    
+    @IBAction func blankChanger(_ sender: UIStepper) {
+        blankDataEntry.value = sender.value
+        updateChartData()
+        
+        
+    }
+    
+    func updateChartData() {
+        
+        let chartDataSet = PieChartDataSet(entries: numberOfDownloadsDataEntries,label: nil)
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let colors = [UIColor(cgColor: #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)), UIColor(cgColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))]
+        chartDataSet.colors = colors as! [NSUIColor]
+        
+        pieChart.data = chartData
+        
+    }
+    
 }
 
