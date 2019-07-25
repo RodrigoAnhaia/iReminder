c465cac1187101876ca22980a25dba16e16de89d
Rodrigo de Anhaia <rodrigoanhaia2@gmail.com>
2019-07-25 12:05:07 -0300

    Fixed some bugs but 1 bug persist

--- iReminder/ButtonGradient.swift
+++ iReminder/ButtonGradient.swift
@@ -6,18 +6,17 @@
 //  Copyright © 2019 Giovani Nícolas Bettoni. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ButtonGradient: UIButton {
+extension UIView {
     
     
-    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor) {
+    func setGradientBackground(colorOne: UIColor, colorTwo: UIColor, colorThree: UIColor) {
         let gradientLayer = CAGradientLayer()
-//        var colorOne.cgColor = #colorLiteral(red: 0.3843137255, green: 0.7098039216, blue: 0.8, alpha: 1)
-//        var colorTwo.cgColor = #colorLiteral(red: 0.2387163937, green: 0.4426490664, blue: 0.5015435219, alpha: 1)
         gradientLayer.frame = bounds
-        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
-        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor, colorThree.cgColor]
+        gradientLayer.locations = [0.0, 0.2,1.0]
         gradientLayer.startPoint = CGPoint(x: 1.0, y : 1.0)
         gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
         
